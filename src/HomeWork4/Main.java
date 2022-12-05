2479f13106af2a9be35d2945f25ee210a7891a38
egorcouldlie <couldliegor@gmail.com>
2022-12-05 10:58:40 +0300

    first exercise

--- src/HomeWork4/Main.java
+++ src/HomeWork4/Main.java
@@ -1,7 +1,15 @@
 package HomeWork4;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Enter your age!");
+        Scanner s1 = new Scanner( System.in);
+        int yourAge = s1.nextInt();
+            if(yourAge >= 18){
+                System.out.println("if your age is " + yourAge + " Than you're an Adult");
+            }else {
+                System.out.println("if your age is " + yourAge + " Than you're not an Adult yet, wait a bit ;)");
+            }
     }
 }
