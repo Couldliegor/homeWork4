8ba86b1edee44ead93199c61fb475d4ce05dbdf0
egorcouldlie <couldliegor@gmail.com>
2022-12-05 21:07:41 +0300

    third exercise

--- src/HomeWork4/Main.java
+++ src/HomeWork4/Main.java
@@ -2,10 +2,10 @@ package HomeWork4;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1() {
         //exercise 1
@@ -22,14 +22,29 @@ public class Main {
     public static void task2(){
         //exercise 2
         System.out.println("\n ");
+        System.out.println("Enter celsius today");
         Scanner sc1 = new Scanner (System.in);
         int temperature = sc1.nextInt();
          if (temperature < 5 ){
-                System.out.println("It's " + temperature + " celcium outside");
+                System.out.println("It's " + temperature + " celcium outside.");
                 System.out.println("The weather cold outside, you need to wear hat!");
          } else {
              System.out.println("It's " + temperature + " celcium outside");
              System.out.println("The weather is warm today, so there is no need to wear hat !");
          }
+        }
+
+    public static void task3(){
+        //exercise 3
+        System.out.println("\n ");
+        System.out.println("What's your speed");
+        Scanner s1 = new Scanner(System.in);
+        int yourSpeed = s1.nextInt();
+            if( yourSpeed > 60){
+                System.out.println("If your speed is " + yourSpeed + ", than you'll have forfeit");
+            }else{
+                System.out.println("If your speed is " + yourSpeed + ", than you dont need to worry!)");
+            }
+
     }
 }
